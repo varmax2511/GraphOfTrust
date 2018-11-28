44c94949dbae8fff59eda1fa94659c471d2c93bd
varmax2511 <var_max@hotmail.com>
2018-11-28 00:58:29 -0500

    - ux

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,11 +1,18 @@
+var flag = false;
 function getAvailableGraphs(){
+	if(flag){
+		return;
+	}
+	
 	$.get("http://localhost:8080/graph/graphs", function(data){
 		for (var i = 0; i < data.length; i++) {
 			var x = document.getElementById("graphs");
 		    var option = document.createElement("option");
 		    option.text = data[i];
 		    x.add(option);
-		}
+		}// for
+		
+		flag = true;
 	});
 	
 }
